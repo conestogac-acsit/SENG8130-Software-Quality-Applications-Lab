9f842d6840b0b5b3cd1f17e6be3029d0f7f29285
raman-dxxp <ramandeep1568@conestogac.on.ca>
2025-05-26 17:34:36 -0400

    Initial commit of sqate-tools

---
+++ sqate-tools/src/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        <div className="bg-gray-100 flex items-center justify-center">
+        <h1 className="text-3xl font-bold text-blue-600">
+          Hello World
+          </h1>
+          </div>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
