af4c1eef4d749b1f5cb9e528897d2a30b925e6e4
Yash Shah <yshah0493@conestogac.on.ca>
2025-05-29 16:43:04 -0400

    ✅ Add TDD tests for SectionList component using Jest + Testing Library

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: ['**/tests/**/*.test.ts?(x)'],
+};
