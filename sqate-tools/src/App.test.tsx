0638749182a39b33b8c1a3a6533c01a68bd5c2f7
raman-dxxp <ramandeep1568@conestogac.on.ca>
2025-05-29 12:20:18 -0400

    Refactored asset structure, cleaned unused CSS, updated manifest icons, moved React packages to devDependencies, and removed unnecessary comments

--- sqate-tools/src/App.test.tsx
+++ sqate-tools/src/App.test.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders App component', () => {
   render(<App />);
+  // Validate the presence of the main title
+  expect(screen.getByText(/SQATE Desktop Tool/i)).toBeInTheDocument();
+  // Validate the presence of the Welcome Message
+   expect(
+    screen.getByText(/Welcome! This is the desktop shell for SQATE tooling modules./i)
+  ).toBeInTheDocument();
 });
