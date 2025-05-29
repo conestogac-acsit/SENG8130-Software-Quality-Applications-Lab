ddffd51faecd2ff5845f27e5c52ce1f68080bd5d
Kuljeet0007 <Kuljeetsingh0977@conestogac.on.ca>
2025-05-29 13:59:37 -0400

    add dynamic URL handling in Electron for dev and production environments - Load React app from localhost:3000 during development - Load build/index.html in production - Added environment detection using app.isPackaged

--- sqate-tools/public/electron.js
+++ sqate-tools/public/electron.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-
+const isDev = !app.isPackaged;
 function createWindow() {
   const win = new BrowserWindow({
     width: 1024,
@@ -11,7 +11,12 @@ function createWindow() {
       contextIsolation: true,
     },
   });
-  win.loadFile(path.join(__dirname, '../build/index.html'));
+
+  if (isDev) {
+    win.loadURL('http://localhost:3000'); // React dev server
+  } else {
+    win.loadFile(path.join(__dirname, '../build/index.html')); // Production build
+  }
 }
 
 app.whenReady().then(createWindow);
