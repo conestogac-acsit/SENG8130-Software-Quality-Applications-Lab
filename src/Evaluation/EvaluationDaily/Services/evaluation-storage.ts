2ebce44ec6f39b53b61e357c9eca9a0700a0c984
Arun <arun@Aruns-MacBook-Pro.local>
2025-06-13 21:20:22 -0400

    evaluation-storage.ts: added logic of evaluation storage

--- src/Evaluation/EvaluationDaily/Services/evaluation-storage.ts
+++ src/Evaluation/EvaluationDaily/Services/evaluation-storage.ts
@@ -1,3 +1,19 @@
 // evaluation-storage.ts
 
 import type { EvaluationRow } from '../Types/EvaluationTypes';
+
+export function loadEvaluationsFromStorage(): EvaluationRow[] {
+    const raw = localStorage.getItem('uploaded-eval-data');
+    if (!raw) return [];
+  
+    try {
+      return JSON.parse(raw) as EvaluationRow[];
+    } catch (err) {
+      console.error('[loadEvaluationsFromStorage] Failed to parse:', err);
+      return [];
+    }
+  }
+  
+  export function saveEvaluationsFromStorage(data: EvaluationRow[]): void {
+    localStorage.setItem('uploaded-eval-data', JSON.stringify(data)); // Save parsed data
+  }
