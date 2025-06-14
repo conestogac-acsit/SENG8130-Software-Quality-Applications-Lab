569247df823a11065ff8af88c710f47c26ebeede
Aayush Thapa <Athapa4126@conestogac.on.ca>
2025-06-14 14:42:40 -0400

    test: update unit test for ModuleNotFound to check dynamic moduleName prop

--- src/components/ModuleNotFound/ModuleNotFound.test.tsx
+++ src/components/ModuleNotFound/ModuleNotFound.test.tsx
@@ -3,8 +3,11 @@ import ModuleNotFound from './index';
  
 describe('ModuleNotFound Component', () => {
   test('renders fallback UI with proper message', () => {
-    render(<ModuleNotFound />);
+    const testModuleName = "TestModule";
+    render(<ModuleNotFound moduleName={testModuleName}/>);
+
     expect(screen.getByText(/Module Not Found/i)).toBeInTheDocument();
+    expect(screen.getByText(/TestModule/i)).toBeInTheDocument();
     expect(screen.getByText(/The module you are trying to access is not available yet/i)).toBeInTheDocument();
   });
 });
