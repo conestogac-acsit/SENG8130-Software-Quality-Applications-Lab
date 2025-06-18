722ae314f14a077a2ca42d129ae26a59a9959c3e
Shyamal <shyamalpatel136@gmail.com>
2025-06-17 20:05:36 -0400

    test: add unit tests for saveUploadedEvaluationData function

--- src/saveUploadedEvaluationService/saveUploadedEvaluationService.test.ts
+++ src/saveUploadedEvaluationService/saveUploadedEvaluationService.test.ts
@@ -15,3 +15,43 @@ const CsvHandler = {
     csvOutput = { fileName, data };
   }
 };
+
+
+function saveUploadedEvaluationData(data: Doo[], fileName: string) {
+  saveToStorage(EVALUATION_DATA_STORAGE_KEY, data);
+  saveToStorage(EVALUATION_FILENAME_STORAGE_KEY, fileName);
+  CsvHandler.saveDataToFile(fileName, data);
+}
+
+describe('saveUploadedEvaluationData', () => {
+  const testData: Doo[] = [
+    {
+      evaluationId: '001',
+      courseCode: 'ENG101',
+      evaluationType: 'Assignment',
+      dueDay: 'Wednesday',
+    },
+  ];
+  
+  const fileName = 'eng101-assignments.csv';
+
+  beforeEach(() => {
+    storage = {};
+    csvOutput = null;
+  });
+
+  it('saves evaluation data to storage with correct key', () => {
+    saveUploadedEvaluationData(testData, fileName);
+    expect(storage[EVALUATION_DATA_STORAGE_KEY]).toEqual(testData);
+  });
+
+  it('saves filename to storage with correct key', () => {
+    saveUploadedEvaluationData(testData, fileName);
+    expect(storage[EVALUATION_FILENAME_STORAGE_KEY]).toBe(fileName);
+  });
+
+  it('calls CsvHandler.saveDataToFile with correct arguments', () => {
+    saveUploadedEvaluationData(testData, fileName);
+    expect(csvOutput).toEqual({ fileName, data: testData });
+  });
+});
