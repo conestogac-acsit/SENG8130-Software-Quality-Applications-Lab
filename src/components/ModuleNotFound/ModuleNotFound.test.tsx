629700a8bf6a0da045ca671ce4526c8b13c394da
Ifteqaruddin Mohammed <Imohammed0631@conestogac.on.ca>
2025-06-18 17:19:46 -0400

    removed comments

--- src/components/ModuleNotFound/ModuleNotFound.test.tsx
+++ src/components/ModuleNotFound/ModuleNotFound.test.tsx
@@ -10,13 +10,9 @@ describe('ModuleNotFound Component', () => {
 <ModuleNotFound />
 </MemoryRouter>
    );
-   // Check for 404 label
    expect(screen.getByLabelText('404 - Page not found')).toBeInTheDocument();
-   // Check for heading text
    expect(screen.getByRole('heading', { name: /Page Not Found/i })).toBeInTheDocument();
-   // Check for descriptive text
    expect(screen.getByText(/we couldn’t find the page/i)).toBeInTheDocument();
-   // Check for the home navigation link
    const homeLink = screen.getByRole('link', { name: /Go Back Home/i });
    expect(homeLink).toBeInTheDocument();
    expect(homeLink).toHaveAttribute('href', '/');
