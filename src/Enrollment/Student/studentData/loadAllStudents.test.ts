677f3039f4cd72544897713635a6c7f282d44d7f
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-06-18 22:43:00 -0400

    Removed test files

--- src/Enrollment/Student/studentData/loadAllStudents.test.ts
+++ src/Enrollment/Student/studentData/loadAllStudents.test.ts
@@ -1,26 +1,7 @@
 import { getAllStudents, StudentDataGetter } from './loadAllStudents'; 
 import { Student } from './studentTypes'; 
 
-const studentInfo: Student[] = [
-  { id: '1', name: 'John Doe', email: 'john@example.com', group: 'A', role: 'Student', section: 'A1', imageUrl: '', notes: '', isLoopEnrolled: true, isGithubEnrolled: false },
-];
-
 describe('getAllStudents', () => {
-  it('should return student data that matches the mock data', () => {
-    const studentGetter: StudentDataGetter = () => studentInfo;
-    const result = getAllStudents(studentGetter);
-    expect(result).toEqual(studentInfo);
-  });
-  it('should return an array', () => {
-    const studentGetter: StudentDataGetter = () => studentInfo;
-    const result = getAllStudents(studentGetter);
-    expect(result).toBeInstanceOf(Array);
-  });
-   it('should return an array with the correct length', () => {
-    const studentGetter: StudentDataGetter = () => studentInfo;
-    const result = getAllStudents(studentGetter);
-    expect(result.length).toBe(1);
-  });
    it('should throw an error if studentGetter returns invalid data (null)', () => {
     const studentGetter: StudentDataGetter = () => null as any;
     expect(() => getAllStudents(studentGetter)).toThrow("Student data is not an array");
