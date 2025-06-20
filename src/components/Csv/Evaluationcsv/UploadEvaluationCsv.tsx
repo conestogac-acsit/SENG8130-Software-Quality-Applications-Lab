0a56130784ca57f2b62e32b2d70fde99012b3362
vraj patel <vrajbp24@gmail.com>
2025-06-19 22:46:00 -0400

    Updated the changes

--- src/components/Csv/Evaluationcsv/UploadEvaluationCsv.tsx
+++ src/components/Csv/Evaluationcsv/UploadEvaluationCsv.tsx
@@ -1,43 +1,41 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useCallback } from "react";
+import Button from "../../Button/Button";
 
 const UploadEvaluationCsv: React.FC = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
-  const handleSelectFile = () => {
+  const handleSelectFile = useCallback(() => {
     fileInputRef.current?.click();
-  };
+  }, []);
+  const handleFileChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const file = e.target.files?.[0] || null;
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0] || null;
-
-    if (file) {
-      if (!file.name.toLowerCase().endsWith(".csv")) {
-        setErrorMessage("File must be a CSV.");
-        setSelectedFile(null);
-      } else {
-        setSelectedFile(file);
-        setErrorMessage(null);
+      if (file) {
+        if (!file.name.toLowerCase().endsWith(".csv")) {
+          setErrorMessage("File must be a CSV.");
+          setSelectedFile(null);
+        } else {
+          setSelectedFile(file);
+          setErrorMessage(null);
+        }
       }
-    }
-  };
-
-  const handleUpload = () => {
+    },
+    []
+  )
+  const handleUpload = useCallback(() => {
     if (selectedFile) {
       console.log("Uploading file:", selectedFile);
       setErrorMessage(null);
-      
     } else {
       setErrorMessage("Please select a CSV file first.");
     }
-  };
-
+  }, [selectedFile]);
   return (
-    <div className="csv-upload-container">
-      <button type="button" onClick={handleSelectFile}>
-        Select CSV File
-      </button>
+    <div>
+       <Button onClick={handleSelectFile} label="Select CSV File" />
       <input
         type="file"
         accept=".csv"
@@ -52,9 +50,7 @@ const UploadEvaluationCsv: React.FC = () => {
         placeholder="No file selected"
       />   
       {selectedFile && (
-        <button type="button" onClick={handleUpload}>
-          Upload
-        </button>
+        <Button onClick={handleUpload} label="Upload" />
       )}
       {errorMessage && (
         <p style={{ color: "red", marginTop: "8px" }}>{errorMessage}</p>
