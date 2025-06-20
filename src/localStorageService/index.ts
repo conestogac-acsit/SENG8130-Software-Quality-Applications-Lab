9f083113197f2c26da440a5c4f15ac1a5f956416
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-06-20 10:38:54 -0400

    index.ts: Export saveOrUpdateEvaluation from index.ts is added.

--- src/localStorageService/index.ts
+++ src/localStorageService/index.ts
@@ -1,2 +1,3 @@
 export { LocalStorage } from './LocalStorage';
 export type { StorageService } from './StorageService';
+export { saveOrUpdateEvaluation } from '../Evaluation/EvaluationService/saveOrUpdateEvaluationService';
