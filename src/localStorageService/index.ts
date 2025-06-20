9ee467ded6d4ed64acaa1bc383c81bd5806f8a73
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-06-20 10:53:26 -0400

    Export is added to the correct index.ts file

--- src/localStorageService/index.ts
+++ src/localStorageService/index.ts
@@ -1,3 +1,2 @@
 export { LocalStorage } from './LocalStorage';
 export type { StorageService } from './StorageService';
-export { saveOrUpdateEvaluation } from '../Evaluation/EvaluationService/saveOrUpdateEvaluationService';
