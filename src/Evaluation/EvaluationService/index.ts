525a8a9b9a51930daaafe7745d43f2ebdb5255f6
Bemzy <bemzy0985@gmail.com>
2025-06-20 10:29:53 -0400

    Added an export function getEvaluationForDate in the already existing index.ts file

--- src/Evaluation/EvaluationService/index.ts
+++ src/Evaluation/EvaluationService/index.ts
@@ -1,2 +1,4 @@
-export { IEvaluationService, EvaluationService } from './EvaluationService';
-export { EvaluationType, Evaluation } from './EvaluationService';
+export { IEvaluationService, EvaluationService } from './EvaluationService'; 
+export { EvaluationType, Evaluation } from './EvaluationService'; 
+
+export { getEvaluationsForDate } from './EvaluationsForDateService';
