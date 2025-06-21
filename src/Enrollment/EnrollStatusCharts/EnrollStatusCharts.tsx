7135c7fdadcbed53da0e97776eaf71f6ebdc3c33
kajal4711 <kajal2108@conestogac.on.ca>
2025-06-20 21:41:27 -0400

    implemented complete functionality for EnrollStatusCharts.tsx

--- src/Enrollment/EnrollStatusCharts/EnrollStatusCharts.tsx
+++ src/Enrollment/EnrollStatusCharts/EnrollStatusCharts.tsx
@@ -17,3 +17,14 @@ interface EnrollStatusChartsProps {
   pieDataLoop: ChartData[];
   barData: BarData[];
 }
+
+<>
+    <div className="flex flex-col lg:flex-row justify-around items-center gap-8">
+      <EnrollmentPieChart title="GitHub Enrollment" data={pieDataGitHub} chartId="githubPieChart" />
+      <EnrollmentPieChart title="Loop Enrollment" data={pieDataLoop} chartId="loopPieChart" />
+    </div>
+    <EnrollmentBarChart data={barData} />
+  </>
+);
+
+export default EnrollStatusCharts;
