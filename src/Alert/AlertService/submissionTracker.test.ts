2af1588c87771ff581fbd779e91bf482e8c434ca
Shyamal <shyamalpatel136@gmail.com>
2025-07-04 14:54:45 -0400

    submissionTracker.test.ts: Test file added for instructor submission status tracking.

---
+++ src/Alert/AlertService/submissionTracker.test.ts
@@ -0,0 +1,6 @@
+describe('getInstructorSubmissionStatus', () => {
+  it('should return "Submitted" for fully completed entries', () => {});
+  it('should return "In Progress" for partially completed entries', () => {});
+  it('should return "Not Started" for instructors with no entries', () => {});
+  it('should return statuses for multiple instructors', () => {});
+});
