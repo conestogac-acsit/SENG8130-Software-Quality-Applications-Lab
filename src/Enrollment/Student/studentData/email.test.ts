4d76e45a9945d5ef44efa9b9a3805916a4f0dae6
NirajanConestoga <nbista7839@conestogac.on.ca>
2025-07-09 19:33:07 -0400

    Update test: Removed references to getValue() after refactoring Email class

--- src/Enrollment/Student/studentData/email.test.ts
+++ src/Enrollment/Student/studentData/email.test.ts
@@ -3,7 +3,7 @@ import { Email } from "./email";
 describe("Email", () => {
   it("should create an Email instance with a valid email", () => {
     const email = new Email("user123@example.com");
-    expect(email.getValue()).toBe("user123@example.com");
+    expect(email.toString()).toBe("user123@example.com");
   });
   it("should throw an error for an invalid email format (missing @)", () => {
     expect(() => {
