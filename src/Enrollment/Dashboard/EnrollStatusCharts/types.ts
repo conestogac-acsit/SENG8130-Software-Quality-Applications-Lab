cd83701d433975776109944cf56246f0d04d90d8
kajal4711 <kajal2108@conestogac.on.ca>
2025-07-14 01:46:36 -0400

    Add types.ts for chart and bar data interfaces

---
+++ src/Enrollment/Dashboard/EnrollStatusCharts/types.ts
@@ -0,0 +1,11 @@
+export interface ChartData {
+  name: string;
+  value: number;
+}
+
+export interface BarData {
+  platform: string;
+  Enrolled: number;
+  Unenrolled: number;
+  Total: number;
+}
