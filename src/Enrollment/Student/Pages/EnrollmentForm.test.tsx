03e8dbf92c2859e1cd44add96c497b413a56e671
Alisha Anzil <Aanzil8281@conestogac.on.ca>
2025-07-15 14:47:14 -0400

    Moved EnrollmentForm and test to Pages under Enrollment/Student and updated Dashboard.tsx

--- src/Enrollment/Student/Pages/EnrollmentForm.test.tsx
+++ src/Enrollment/Student/Pages/EnrollmentForm.test.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import EnrollmentForm from './EnrollmentForm';
 
 describe('EnrollmentForm Component', () => {
-  it('renders all fields and buttons', () => {
-    const { getByPlaceholderText, getByText } = render(<EnrollmentForm />);
-
-    expect(getByPlaceholderText('First Name')).toBeInTheDocument();
-    expect(getByPlaceholderText('Last Name')).toBeInTheDocument();
-    expect(getByPlaceholderText('Student ID')).toBeInTheDocument();
-    expect(getByPlaceholderText('Course Name')).toBeInTheDocument();
-    expect(getByPlaceholderText('Email ID')).toBeInTheDocument();
-
-    expect(getByText('Enroll in GitHub')).toBeInTheDocument();
-    expect(getByText('Submit')).toBeInTheDocument();
+  it('renders the heading inside CenteredFormContainer', () => {
+    render(<EnrollmentForm />);
+    const heading = screen.getByText('Student Enrollment Form');
+    expect(heading).toBeInTheDocument();
   });
 });
--- src/Enrollment/Student/Pages/EnrollmentForm.tsx
+++ src/Enrollment/Student/Pages/EnrollmentForm.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import CenteredFormContainer from '../../../Components/CenteredFormContainer/CenteredFormContainer';
 
-
 const EnrollmentForm = () => {
-  return(
-
-
+  return (
+    <CenteredFormContainer>
       <h1>Student Enrollment Form</h1>
-
+    </CenteredFormContainer>
   );
-
 };
 
 export default EnrollmentForm;
