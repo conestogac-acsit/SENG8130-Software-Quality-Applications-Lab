976641607aade2606ff244c0cd28882fcc1fa825
Alisha Anzil <Aanzil8281@conestogac.on.ca>
2025-07-15 14:33:13 -0400

    Moved EnrollmentForm and test to Pages under Enrollment/Student and updated Dashboard.tsx

---
+++ src/Enrollment/Student/Pages/EnrollmentForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CenteredFormContainer from '../../../Components/CenteredFormContainer/CenteredFormContainer';
+
+
+const EnrollmentForm = () => {
+  return(
+
+
+      <h1>Student Enrollment Form</h1>
+
+  );
+
+};
+
+export default EnrollmentForm;
