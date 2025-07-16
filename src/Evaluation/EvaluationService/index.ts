75c731b2d1317056d3d8ffe65c2a009768357831
ValeriaGallegoR <valeriagallego20@gmail.com>
2025-07-16 18:31:24 -0400

    Index.ts: add export to integrate getEvaluationsForCampus.

--- src/Evaluation/EvaluationService/index.ts
+++ src/Evaluation/EvaluationService/index.ts
@@ -1,3 +1,5 @@
 export { IEvaluationService, EvaluationService } from './EvaluationService';
 export { EvaluationType, Evaluation } from './EvaluationService';
 export { saveOrUpdateEvaluation } from './saveOrUpdateEvaluationService';
+
+export { getEvaluationsForCampus  } from "./EvaluationsForCampusService";
