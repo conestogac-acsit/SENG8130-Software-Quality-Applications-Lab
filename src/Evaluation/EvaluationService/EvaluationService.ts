7ec8c04a8907b4be24ab10471f478b989a13d35d
root <root@LAPTOP-33N12T6F.>
2025-07-15 22:21:27 -0400

    Added original code back to EvaluationService.ts

--- src/Evaluation/EvaluationService/EvaluationService.ts
+++ src/Evaluation/EvaluationService/EvaluationService.ts
@@ -43,12 +43,9 @@ export class EvaluationService implements IEvaluationService {
   
   loadEvaluations(): Evaluation[] {
     try {
-      const data = this.storageService.load<any[]>(EVALUATION_DATA_STORAGE_KEY);
+      const data = this.storageService.load<Evaluation[]>(EVALUATION_DATA_STORAGE_KEY);
       if (data) {
-        return data.map(ev => ({
-          ...ev,
-          dueDate: new Date(ev.dueDate)  
-        }));
+        return data;
       } else {
         console.warn('No evaluations found in storage');
         return [];
