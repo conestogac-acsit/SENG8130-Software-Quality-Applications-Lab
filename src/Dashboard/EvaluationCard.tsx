2f05c0090ab0088b423f96b19c47f799b148353a
Prayerson16 <pchauhan8082@conestogac.on.ca>
2025-07-17 19:16:42 -0400

    feat: add heatmap link in the EvaluationCard

--- src/Dashboard/EvaluationCard.tsx
+++ src/Dashboard/EvaluationCard.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { EvaluationService } from "../Evaluation/EvaluationService/EvaluationService";
 import { LocalStorage } from "../localStorageService";
-import type { StorageService } from "../localStorageService"; 
+import type { StorageService } from "../localStorageService";
 import { Evaluation } from "../Evaluation/EvaluationService/EvaluationService";
 
 const EvaluationCard = () => {
@@ -16,16 +16,33 @@ const EvaluationCard = () => {
   }, []);
 
   return (
-    <div
-      className="rounded-xl bg-white shadow p-6 text-center cursor-pointer hover:bg-green-100 transition"
-      onClick={() => navigate("/evaluation")}
-    >
-      <h2 className="text-xl font-semibold text-gray-800">Evaluation Service</h2>
-      <p className="text-gray-500 text-sm mt-2">
-        {evaluations.length > 0
-          ? `${evaluations.length} evaluations available`
-          : "No evaluations yet"}
-      </p>
+    <div className="rounded-xl bg-white shadow p-6 text-cente">
+      <div
+        className="cursor-pointer hover:bg-green-100 transition"
+        onClick={() => navigate("/evaluation")}
+      >
+        <h2 className="text-xl font-semibold text-gray-800">
+          Evaluation Service
+        </h2>
+        <p className="text-gray-500 text-sm mt-2">
+          {evaluations.length > 0
+            ? `${evaluations.length} evaluations available`
+            : "No evaluations yet"}
+        </p>
+      </div>
+      <div
+        className="cursor-pointer hover:bg-green-100 transition"
+        onClick={() => navigate("/heatmap")}
+      >
+        <h2 className="text-xl font-semibold text-gray-800">
+          Evaluation Heatmap
+        </h2>
+        <p className="text-gray-500 text-sm mt-2">
+          {evaluations.length > 0
+            ? `${evaluations.length} evaluations available`
+            : "No evaluations yet"}
+        </p>
+      </div>
     </div>
   );
 };
