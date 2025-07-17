84c22e1f9983ba6506fd53b27f9d442188ffee2e
shivaamanaganti <shiva30jun@gmail.com>
2025-07-17 13:17:07 -0400

    feat: add StudentList component route under enrollment module

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ModuleNotFound from './Components/ModuleNotFound'; // adjust path if needed
-import { StudentList } from './Enrollment/Student/Pages/StudentList';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import logo from './assets/logo.png';
 import ModuleNotFound from './Components/ModuleNotFound'; 
 import EnrollmentDashboard from './Enrollment/Dashboard/Dashboard';
 import EvaluationCard from '../src/Dashboard/EvaluationCard';
+import { StudentList } from './Enrollment/Student/Pages/StudentList';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/enrollment" element={<EnrollmentDashboard />} />
+        <Route path="students" element={<StudentList />} />
         <Route path="/evaluation" element={<EvaluationCard />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<ModuleNotFound />} />
