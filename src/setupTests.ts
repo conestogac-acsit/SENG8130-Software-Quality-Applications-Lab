7002d16663e91f950e15889fb11383d081bb46f5
Yash Shah <yshah0493@conestogac.on.ca>
2025-07-17 13:50:07 -0400

    Resolved conflict in setupTests.ts by applying safe polyfill for TextEncoder/Decoder

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,3 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
 // Polyfill TextEncoder and TextDecoder (needed by some libraries like react-router in Jest/Node)
