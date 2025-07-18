95f328870ba41910b820fa162bafd1f2151ee37f
PrayersonChauhan <pchauhan8082@conestogac.on.ca>
2025-07-18 14:05:55 -0400

    Issue 402 create heatmap UI (#568)
    * feat: add heatmap  UI page
    * test: add heatmap link test in the EvaluationCard test
    * feat: add heatmap link in the EvaluationCard
    * feat: add index.ts for Components export

--- src/Dashboard/EvaluationCard.tsx
+++ src/Dashboard/EvaluationCard.tsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { EvaluationService } from "../Evaluation/EvaluationService/EvaluationService";
 import { LocalStorage } from "../localStorageService";
-import type { StorageService } from "../localStorageService"; 
+import type { StorageService } from "../localStorageService";
 import { Evaluation } from "../Evaluation/EvaluationService/EvaluationService";
+import {Heatmap} from "../Evaluation/Heatmap";
 
 const EvaluationCard = () => {
   const navigate = useNavigate();
@@ -16,16 +17,32 @@ const EvaluationCard = () => {
   }, []);
 
   return (
-    <div
-      className="rounded-xl bg-white shadow p-6 text-center cursor-pointer hover:bg-green-100 transition"
-      onClick={() => navigate("/evaluation")}
-    >
-      <h2 className="text-xl font-semibold text-gray-800">Evaluation Service</h2>
-      <p className="text-gray-500 text-sm mt-2">
-        {evaluations.length > 0
-          ? `${evaluations.length} evaluations available`
-          : "No evaluations yet"}
-      </p>
+    <div className="rounded-xl bg-white shadow p-6 text-cente">
+      <div
+        className="cursor-pointer hover:bg-green-100 transition"
+        onClick={() => navigate("/evaluation")}
+      >
+        <h2 className="text-xl font-semibold text-gray-800">
+          Evaluation Service
+        </h2>
+        <p className="text-gray-500 text-sm mt-2">
+          {evaluations.length > 0
+            ? `${evaluations.length} evaluations available`
+            : "No evaluations yet"}
+        </p>
+      </div>
+      <div
+        className="cursor-pointer hover:bg-green-100 transition"
+      >
+        <h2 className="text-xl font-semibold text-gray-800">
+          Evaluation Heatmap
+        </h2>
+        <p className="text-gray-500 text-sm mt-2">
+          {evaluations.length > 0
+            ? <Heatmap />
+            : "No evaluations yet"}
+        </p>
+      </div>
     </div>
   );
 };
