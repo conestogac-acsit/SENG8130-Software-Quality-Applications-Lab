1755ed5ad04bdb5376bf48bac7826fa974a3ff50
Aayush Thapa <Athapa4126@conestogac.on.ca>
2025-07-31 21:47:14 -0400

    Removed unused componentDidCatch

--- src/Components/ErrorBoundary/ErrorBoundary.tsx
+++ src/Components/ErrorBoundary/ErrorBoundary.tsx
@@ -12,12 +12,8 @@ interface State {
 class ErrorBoundary extends Component<Props, State> {
   state: State = { hasError: false };
 
-  static getDerivedStateFromError(): State {
+  static getDerivedStateFromError(error: Error): State {
     return { hasError: true };
-  }
-
-  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error("Error caught by ErrorBoundary:", error, errorInfo);
   }
 
   resetError = () => {
