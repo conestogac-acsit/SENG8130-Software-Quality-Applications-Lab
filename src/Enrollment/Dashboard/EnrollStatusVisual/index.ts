69aa798b27eada9f2f664b2df0bb241685ec1775
kajal4711 <kajal2108@conestogac.on.ca>
2025-08-02 22:42:33 -0400

    Implemented index.ts

---
+++ src/Enrollment/Dashboard/EnrollStatusVisual/index.ts
@@ -0,0 +1,3 @@
+export { default as EnrollStatusVisual } from './EnrollStatusVisual';
+export { default as EnrollStatusCharts } from './EnrollStatusCharts';
+export type { ChartData, BarData } from './EnrollStatusCharts';
