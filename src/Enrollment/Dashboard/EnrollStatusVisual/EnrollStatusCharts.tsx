73b1920afc70eb15ffc826fefa8afd13f2a56602
kajal4711 <kajal2108@conestogac.on.ca>
2025-08-02 22:37:23 -0400

    Implemented EnrollStatusCharts and all the test cases passed

---
+++ src/Enrollment/Dashboard/EnrollStatusVisual/EnrollStatusCharts.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export interface ChartData {
+  name: string;
+  value: number;
+}
+export interface BarData {
+  platform: string;
+  Enrolled: number;
+  Unenrolled: number;
+  Total: number;
+}
+
+interface EnrollStatusChartsProps {
+  pieDataGitHub: ChartData[];
+  pieDataLoop: ChartData[];
+  barData: BarData[];
+}
+
+const EnrollStatusCharts: React.FC<EnrollStatusChartsProps> = ({
+  pieDataGitHub,
+  pieDataLoop,
+  barData
+}) => (
+  <>
+    <div className="flex flex-col lg:flex-row justify-around items-center gap-8">
+      <EnrollmentPieChart title="GitHub Enrollment" data={pieDataGitHub} chartId="githubPieChart" />
+      <EnrollmentPieChart title="Loop Enrollment" data={pieDataLoop} chartId="loopPieChart" />
+    </div>
+    <EnrollmentBarChart data={barData} />
+  </>
+);
+
+export default EnrollStatusCharts;
