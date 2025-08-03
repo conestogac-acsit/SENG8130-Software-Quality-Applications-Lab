32da59165f0fbe7540f21b8c74fa41c539ccf0db
Rajesh <rgulivindala1146@conestoga.on.ca>
2025-08-03 12:08:00 -0400

    Updated index.ts

--- src/Enrollment/Dashboard/EnrollStatusVisual/index.ts
+++ src/Enrollment/Dashboard/EnrollStatusVisual/index.ts
@@ -1,3 +1,4 @@
 export { default as EnrollStatusExports } from './EnrollStatusExports';
 export { default as ExportButton } from './ExportButton';
 export { useExportToPng } from './useExportToPng';
+export { default as EnrollStatusVisual } from './EnrollStatusVisual';
